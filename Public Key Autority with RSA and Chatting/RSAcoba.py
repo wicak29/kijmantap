fa3aacaee322d29545ffa9cff2e2ede675225cf4
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-05-16 11:28:54 +0800

    message conversion

--- Public Key Autority with RSA and Chatting/RSAcoba.py
+++ Public Key Autority with RSA and Chatting/RSAcoba.py
@@ -1,7 +1,7 @@
 __author__ = 'Indra Gunawan'
 __author__ = 'Dwi Pratama'
 
-import random
+import random, math
 from fractions import gcd
 
 simpanCoprime =[]
@@ -22,6 +22,27 @@ def isPrime(n):
          k += 2
     return True
 
+def intcaststr(bitlist):
+    return int("".join(str(i) for i in bitlist), 2)
+
+
+def bitfield(n):
+    return [1 if digit=='1' else 0 for digit in bin(n)[2:]]
+
+def str_ke_bitlist(s):
+    ords = (ord(c) for c in s)
+    shifts = (7, 6, 5, 4, 3, 2, 1, 0)
+    return [(o >> shift) & 1 for o in ords for shift in shifts]
+
+def bitlist_ke_chars(bl):
+    bi = iter(bl)
+    bytes = zip(*(bi,) * 8)
+    shifts = (7, 6, 5, 4, 3, 2, 1, 0)
+    for byte in bytes:
+        yield chr(sum(bit << s for bit, s in zip(byte, shifts)))
+
+def bitlist_ke_str(bl):
+    return ''.join(bitlist_ke_chars(bl))
 def coprime (EulerTotient, N):
     for index in range(2,EulerTotient):
         if gcd(index,N) == 1:
@@ -51,12 +72,30 @@ euler_totient = (P_Value-1)*(Q_Value-1)
 #coprime(euler_totient, N_Value)
 privateKeyD, publicKeyE = coprime(euler_totient, N_Value)
 
+#==========Message conversion====
+hel = "a"
+hel2 = str_ke_bitlist(hel)
+hel3 = intcaststr(hel2)
+m = hel3
 
-print P_Value
-print Q_Value
-print "p*q = "+ str(N_Value)
-print "euler totient = "+ str(euler_totient)
+c = (m**publicKeyE)%N_Value
+print c
+decript = (c**privateKeyD)%N_Value
+print decript
+print chr(decript)
+# hel4 = bitfield(decript)
+# hel5 = bitlist_ke_str(hel4)
+
+#print hel5
+
+print m
+print N_Value
+
+#print P_Value
+#print Q_Value
+#print "p*q = "+ str(N_Value)
+#print "euler totient = "+ str(euler_totient)
 #print "show coprime = ", simpanCoprime
-print "Private key D = ", privateKeyD
-print "Public key E  = ", publicKeyE
+#print "Private key D = ", privateKeyD
+#print "Public key E  = ", publicKeyE
 #print "show biggest factor = " + str(biggestFactor)
