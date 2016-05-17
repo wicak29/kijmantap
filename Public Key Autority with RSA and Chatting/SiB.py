8bd3905481b599eae369aa8e296c4b5400317ea7
arijayadwi <arijayadwi@gmail.com>
2016-05-17 11:51:05 +0700

    tambah A dan B

--- Public Key Autority with RSA and Chatting/SiB.py
+++ Public Key Autority with RSA and Chatting/SiB.py
@@ -1,11 +1,10 @@
 __author__ = 'Indra Gunawan'
-#server
+#client
 import socket
 import threading
-import time
 from suds.client import Client
 
-SIZE = 4
+SIZE =4
 
 #-------------------------------- RSA START ---------------------------------------------------------------------
 
@@ -132,40 +131,29 @@ print publicKeyAuth[1]
 publicKeyA = str(publicKeyE)+'>'+str(N_Value)
 Certificate = client.service.request(publicKeyA)
 
-print Certificate
-
-soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-soc.bind(('127.0.0.1',5432))
-soc.listen(5)
-
-class CThread(threading.Thread):
+class client(threading.Thread):
     def __init__(self,c):
         threading.Thread.__init__(self)
         self.conn = c
-        self.stopIt=False
+        self.stopIt = False
 
     def mrecv(self):
         data = self.conn.recv(SIZE)
         self.conn.send('OK')
-        msg = self.conn.recv(int(data))
-        return msg
+        return self.conn.recv(int(data))
 
     def run(self):
         while not self.stopIt:
             msg = self.mrecv()
-            print 'recieved->  ',msg
+            print 'recieved-> ',msg
 
-def setConn(con1,con2):
-    dict={}
-    state = con1.recv(9)
-    con2.recv(9)
-    if state =='WILL RECV':
-        dict['send'] = con1 # server will send data to reciever
-        dict['recv'] = con2
-    else:
-        dict['recv'] = con1 # server will recieve data from sender
-        dict['send'] = con2
-    return dict
+soc1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+soc1.connect(('127.0.0.1',5432))
+soc1.send('WILL SEND') # telling server we will send data from here
+
+soc2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+soc2.connect(('127.0.0.1',5432))
+soc2.send('WILL RECV') # telling server we will recieve data from here
 
 def msend(conn,msg):
     if len(msg)<=999 and len(msg)>0:
@@ -177,19 +165,15 @@ def msend(conn,msg):
         if conn.recv(2) == 'OK':
             conn.send(msg[:999])
             msend(conn,msg[1000:]) # calling recursive
-
-
-(c1,a1) = soc.accept()
-(c2,a2) = soc.accept()
-dict = setConn(c1,c2)
-thr = CThread(dict['recv'])
+thr = client(soc2)
 thr.start()
 try:
     while 1:
-        msend(dict['send'],raw_input())
+        msend(soc1,raw_input())
 except:
     print 'closing'
 thr.stopIt=True
-msend(dict['send'],'bye!!!')# for stoping the thread
+msend(soc1,'bye!!') # for stoping the thread
 thr.conn.close()
-soc.close()
+soc1.close()
+soc2.close()
